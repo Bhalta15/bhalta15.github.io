d89d61bc564dac5b16cba8c4ae2c27ba69174c66
bltzr05 <urbahe15@gmail.com>
2026-04-08 16:07:34 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,56 +1,43 @@
-const CACHE_NAME = "daily-love-v1";
+const CACHE_NAME = "daily-love-v2";
 
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/principal.html",
-  "/secundario.html",
-  "/registro.html",
-
-  // JS
-  "/js/firebase.js",
-  "/js/index.js",
-  "/js/principal.js",
-  "/js/secundario.js",
-  "/js/registro.js",
-  "/js/registroFirebase.js",
-
-  // Imágenes
-  "/icono-mensajes.jpg",
-  "/DailyLove.png"
+  "/DailyLove/",
+  "/DailyLove/index.html",
+  "/DailyLove/principal.html",
+  "/DailyLove/secundario.html",
+  "/DailyLove/registro.html",
+  "/DailyLove/js/firebase.js",
+  "/DailyLove/js/principal.js",
+  "/DailyLove/js/secundario.js",
+  "/DailyLove/js/registro.js",
+  "/DailyLove/js/registroFirebase.js",
+  "/DailyLove/js/toast.js",
+  "/DailyLove/DailyLoveN.png"
 ];
 
-// INSTALAR
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
 });
 
-// ACTIVAR
 self.addEventListener("activate", e => {
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
         keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
+          if (key !== CACHE_NAME) return caches.delete(key);
         })
       )
-    )
+    ).then(() => self.clients.claim())
   );
 });
 
-// FETCH
 self.addEventListener("fetch", e => {
   e.respondWith(
     caches.match(e.request)
       .then(res => res || fetch(e.request))
   );
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
 });
